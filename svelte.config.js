0582ecff00a367f1363036f7da05734b2b387b53
Manoel <manoelcl@gmail.com>
2025-09-22 14:42:15 +0200

    remove adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -21,16 +21,16 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		paths: { relative: false },
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		// paths: { relative: false },
+		// adapter: adapter({
+		// 	// default options are shown. On some platforms
+		// 	// these options are set automatically — see below
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: undefined,
+		// 	precompress: false,
+		// 	strict: true
+		// })
 	}
 };
 
